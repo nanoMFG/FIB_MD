a2d4b1e61721a71098365d0b6b5fd4391d091002
joshmstout <josh@wirelessprv-10-192-133-252.near.illinois.edu>
2018-01-24 16:59:17 -0600

    Cleaned up remnant .dat, .out files: round 1

--- batchsub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-JOB=`qsub jobsub`
-
-for a in `seq 9`
-do
-  JOB=`qsub -W depend=afterany:$JOB jobsub`
-done
--- batchsub_xsede.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-JOB=`sbatch job.mpi`
-
-for a in `seq 5`
-do
-  JOB=`sbatch --dependency=afterany:$JOB job.mpi`
-done
